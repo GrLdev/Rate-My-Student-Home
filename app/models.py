0ad3f7b0bb3cbc6d587e530223b016b396beae85
Mohammed Ahmed <AhmedMA10@cardiff.ac.uk>
2023-05-18 18:18:44 +0100

    added Property incl House and Halls models

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
     property_id = db.Column(db.Integer, db.ForeignKey('property.id'), nullable=False)
-    estate_agent_id = db.Column(db.Integer, db.ForeignKey('estate_agent.id'), nullable=False)
+    estate_agent_id = db.Column(db.Integer, db.ForeignKey('estate_agent.id'))
     overall_rating = db.Column(db.Integer, nullable=False)
     condition_rating = db.Column(db.Integer, nullable=False)
     security_rating = db.Column(db.Integer, nullable=False)
@@ -14,3 +14,20 @@ class Review(db.Model):
 
     def __repr__(self):
         return f"Review(id='{self.id}', user_id='{self.user_id}', property_id='{self.property_id}', estate_agent_id='{self.estate_agent_id}', overall_rating='{self.overall_rating}', condition_rating='{self.condition_rating}', security_rating='{self.security_rating}', landlord_rating='{self.landlord_rating}', date='{self.date}', comment='{self.comment}')" 
+
+class Property(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    place_id = db.Column(db.String(256), nullable=False)
+    address = db.Column(db.String(256), nullable=False)
+
+class House(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    property_id = db.Column(db.Integer, db.ForeignKey('property.id'), nullable=False)
+    bedrooms = db.Column(db.Integer, nullable=False)
+    bathrooms = db.Column(db.Integer, nullable=False)
+    rent = db.Column(db.Integer, nullable=False)
+
+class Halls(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    property_id = db.Column(db.Integer, db.ForeignKey('property.id'), nullable=False)
+    rent = db.Column(db.Integer, nullable=False)
